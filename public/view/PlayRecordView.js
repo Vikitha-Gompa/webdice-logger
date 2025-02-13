67790f32017c8233c5f6d667369e220e0226d36a
Vikitha <vikithagompa@gmail.com>
2025-02-13 17:49:01 -0600

    final commit

--- public/view/PlayRecordView.js
+++ public/view/PlayRecordView.js
@@ -1,6 +1,7 @@
 import {currentUser} from "../controller/firebase_auth.js";
 import { AbstractView } from "./AbstractView.js";
 import { startSpinner, stopSpinner } from "./util.js";
+import {GamePlayStrategy, marking} from "../model/HomeModel.js";
 
 export class PlayRecordView extends AbstractView{
     controller = null;
@@ -36,8 +37,50 @@ export class PlayRecordView extends AbstractView{
     async updateView(){
         console.log('ProfileView.updateView() called');
         const viewWrapper = document.createElement('div');
-        viewWrapper.innerHTML=  JSON.stringify(this.controller.model.playRecords);
+        const response = await fetch('/view/templates/playrecord.html'); { cache: 'no-store' };
+        viewWrapper.innerHTML = await response.text();
+
+        viewWrapper.querySelector('#player').textContent = 'Player:' + currentUser.email;
+        const playRecords = this.controller.model.playRecords;
+        const playRecordMain = viewWrapper.querySelector('#playRecordMain');
+        if(playRecords == null){
+            playRecordMain.innerHTML = '<h2>Error loading play records</h2>';
+        } else if(playRecords.length == 0){
+            playRecordMain.innerHTML = '<h2>No play records found</h2>';
+        } else{
+            for(const record of playRecords){
+                const recordView = this.buildCollapsibleRecord(record);
+                playRecordMain.appendChild(recordView);
+            }
+        }
+
+
         return viewWrapper;
+    }
+
+    buildCollapsibleRecord(record){
+        const topDiv = document.createElement('div');
+        topDiv.classList.add('mb-2');
+
+        const a = document.createElement('a');
+        a.classList.add('btn', 'btn-secondary', 'btn-sm');
+        a.setAttribute('data-bs-toggle', 'collapse');
+        a.href = `#record${record.docId}`;
+        a.textContent = new Date(record.timestamp).toLocaleString();
+        topDiv.appendChild(a);
+
+        const collapseDiv = document.createElement('div');
+        collapseDiv.classList.add('collapse');
+        collapseDiv.id = `record${record.docId}`;
+        collapseDiv.innerHTML = `
+            <div class="card card-body">
+               <p>Game Strategy: ${record.gameStrategy == GamePlayStrategy.VS_HUMAN ? 'Human vs. Human' : 'Human vs. Random'}</p>
+               <p>Moves: ${record.moves}</p>
+               <p>Winner: ${record.winner == marking.U ? 'Draw' : record.winner}</p>
+            </div>
+        `;
+        topDiv.appendChild(collapseDiv);
+        return topDiv;
     }
 
     attachEvents(){
