3cdeda404845b62de905c4eb88fadde3410197d0
Vikitha <vikithagompa@gmail.com>
2025-03-15 12:19:44 -0500

    final commit

--- public/controller/firebase_auth.js
+++ public/controller/firebase_auth.js
@@ -8,7 +8,7 @@ import {
 
 import { app } from './firebase_core.js';
 import { router } from "./app.js";
-import { glHomeModel } from "./HomeController.js";
+import { glHomeModel } from "./HomeController1.js";
 
 
 const auth = getAuth(app);
@@ -47,7 +47,7 @@ onAuthStateChanged(auth, user => {
         spaRoot.classList.replace('d-block', 'd-none');
         router.currentView = null;
         spaRoot.innerHTML = '';  // claer the view
-        document.getElementById("logininf")
+        document.getElementById("logininfo")
         glHomeModel.reset();
     }
  
